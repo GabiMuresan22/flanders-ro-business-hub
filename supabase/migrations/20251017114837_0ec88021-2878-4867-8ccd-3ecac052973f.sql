00b4211f81da3296e1a4858ab3891ccd8653bdea
Gabi <gabimuresan2289@gmail.com>
2025-10-18 00:10:07 +0200

    delete database

--- supabase/migrations/20251017114837_0ec88021-2878-4867-8ccd-3ecac052973f.sql
+++
@@ -1,9 +0,0 @@
--- Drop the existing restrictive INSERT policy
-DROP POLICY IF EXISTS "Anyone can submit a business" ON businesses;
-
--- Create a new permissive INSERT policy for authenticated and anonymous users
-CREATE POLICY "Allow anyone to submit businesses"
-ON businesses
-FOR INSERT
-TO public
-WITH CHECK (true);
