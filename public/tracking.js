bd5d1ab0db8c0b957336d16b9e18d65c5cc092ee
Gabi <gabimuresan2289@gmail.com>
2026-02-19 06:56:43 +0100

    Strengthen CSP: remove unsafe-inline from script-src, move gtag stub to external tracking.js

---
+++ public/tracking.js
@@ -0,0 +1,8 @@
+/**
+ * Google Analytics gtag stub - must load before GTM/analytics scripts.
+ * Inline script moved to external file for strict CSP (no 'unsafe-inline').
+ */
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
